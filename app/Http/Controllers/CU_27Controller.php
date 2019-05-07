70d64d3b2f91337586522451a5010182b9bf49dc
Xavier Franch <xavi.franchtarazaga@gmail.com>
2019-05-07 13:24:35 +0200

    Editar plantilla -> funcionament correcte

--- app/Http/Controllers/CU_27Controller.php
+++ app/Http/Controllers/CU_27Controller.php
@@ -1,60 +1,41 @@
 <?php
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Usuari;
 use \App\crearPlantilla;
 use App\plantillaRevisor;
-class CU_27Controller extends Controller {
-public function getIndex($id){
-       $plantillas = crearPlantilla::findOrFail($id);
-       $userAprov = Usuari::all();
-       $usersRev = plantillaRevisor::all();
-       return view('CU_27_EditarPlantilla_modal', compact('plantillas', 'userAprov', 'usersRev','id'));
-}
-public function editarPlantilla(Request $request, $id) {
+
+class CU_27Controller extends Controller
+{
+    public function getIndex($id)
+    {
+        $plantillas = crearPlantilla::findOrFail($id);
+        $userAprov = Usuari::all();
+        $usersRev = plantillaRevisor::all();
+        return view('CU_27_EditarPlantilla_modal', compact('plantillas', 'userAprov', 'usersRev', 'id'));
+    }
+    public function editarPlantilla(Request $request, $id)
+    {
         var_dump($id);
         session_start();
-       $plantillas = crearPlantilla::findOrFail($id);
-       
-       
-       $plantillas->nomPlantilla= $request->nomPlantilla;
-        $plantillas->idUsuariAprovador= $request->aprov;
-       
-       $plantillas->idUsuariCreador= $_SESSION['idUsuari'];
-       $plantillas->save();
-        
-        $plantirevisors = plantillaRevisor::findOrFail($id);
-        
-//      print_r(Usuari::where('nomUsuari','=',)->first()->idUsuari);
-         $plantirevisors->idUsuariRevisor = $request->revi[0];
-        
-        //$plantirevisors->idUsuariRevisor=$request->revi4;
-        
-        /*if (revi[0] == null){
-            $plantirevisors->idUsuariRevisor = $request->revi4[0];
-        } else{
-            $plantirevisors->idUsuariRevisor=$request->revi[0];
-        }*/
-        
-        $plantirevisors->save();
-        
- 
-      
-        return redirect ('/CU_50');
-       //return view('/CU_50');
-       //
-//       $plantilla->nomPlantilla= $request->nomPlantilla;
-//       $plantilla->idUsuariAprovador= $request->aprov;
-//       $plantilla->idUsuariCreador= $_SESSION['idUsuari'];
-//       $plantilla->save();
-//       
-//       $plantirevisors->idUsuariRevisor= $request->revi;
-//       $plantirevisors->idPlantilla=$plantilla->idPlantilla;
-//       $plantirevisors->save();
-       
-      
-      //return redirect ('/CU_50');
-    }
-    
-}
 
+        // Busquem la plantilla i editem id i aprovador
+        $plantillas = crearPlantilla::findOrFail($id);
+        $plantillas->nomPlantilla= $request->nomPlantilla;
+        $plantillas->idUsuariAprovador= $request->aprov;
+        $plantillas->idUsuariCreador= $_SESSION['idUsuari'];
+        $plantillas->save();
+
+        // Busquem els revisors de la plantilla, els eliminem i afgim els nous
+        $plantirevisors = plantillaRevisor::where('idPlantilla', $id)->delete();
+        foreach ($request->revi as $revi) {
+            $revisorPlantilla = new plantillaRevisor;
+            $revisorPlantilla->idUsuariRevisor = $revi;
+            $revisorPlantilla->idPlantilla = $id;
+            $revisorPlantilla->save();
+        }
+
+        return redirect('/CU_50');
+    }
+}
--- resources/views/CU_27_EditarPlantilla_modal.blade.php
+++ resources/views/CU_27_EditarPlantilla_modal.blade.php
@@ -13,65 +13,54 @@
 			</div>
 
 			<div class="modal-body">
-
 				<form method="POST" action="{{url('/CU_27_EditarPlantilla/'.$id  )}}">
 					{{ csrf_field() }}
+
 					<div class="form-group">
 						<label for="title" class="control-label">Nom</label>
 						<input type="text" class="form-control" name="nomPlantilla" id="nomPlantilla" value="{{ $nomPlantilla }}">
 					</div>
-					<label for="Aprovador" class="control-label">Aprovador seleccionat</label>
+
 					<div class="form-group">
-						@foreach($userAprov as $user)
-							@if ($plantillas->idUsuariAprovador == $user->idUsuari)
-
-								<input type="text" class="form-control" readonly="readonly" name="aprov" id="aprov" value="{{ $UsuariAprovador }}"><br>
-
-							@endif
-						@endforeach
-						
 						<label for="re" class="control-label">Aprovador</label>
 						<select class="form-control" name="aprov">
 							@foreach($userAprov as $user)
-							{{ $user->idUsuari }}
-
-							<option value="{{ $user->idUsuari }}" <?php if($UsuariAprovador==$user->nomUsuari ) echo "selected" ?>> {{ $user->nomUsuari }}</option>
+								{{ $user->idUsuari }}
+								<option value="{{ $user->idUsuari }}" <?php if($UsuariAprovador==$user->nomUsuari ) echo "selected" ?>> {{ $user->nomUsuari }}</option>
 							@endforeach
 						</select>
 					</div>
 
-					<label for="Aprovador" class="control-label">Revisor/s actuals</label>
+					<?php $revidoresActuales[] = array()?>
+					@foreach($usersRev as $revi)
+						@foreach($userAprov as $user)
+							@if ($plantillas->idPlantilla == $revi->idPlantilla)
+								@if($revi->idUsuariRevisor == $user->idUsuari)
+									<?php  array_push($revidoresActuales, $user->nomUsuari )?>
+								@endif
+							@endif
+						@endforeach
+					@endforeach
 
 					<div class="form-group">
-						<?php $revidoresActuales[] = array()?>
-						@foreach($usersRev as $revi)
-							@foreach($userAprov as $user)
-								@if ($plantillas->idPlantilla == $revi->idPlantilla)
-									@if($revi->idUsuariRevisor == $user->idUsuari)
-										<?php  array_push($revidoresActuales, $user->nomUsuari )?>
-										<input type="text" class="form-control" readonly="readonly" id="revi" name="revi4[]" value="{{ $user->nomUsuari }}">
-									@endif
-								@endif
-							@endforeach
-						@endforeach
-						<br>
-						<label for="re" class="control-label">Revisor/es</label>
-						<select class="form-control" multiple size="3" name="revi[]">
+						<label for="re" class="control-label">Revisor/s</label>
+						<select class="form-control" multiple size="5" name="revi[]" id="re">
 							@foreach($userAprov as $user)
 								<option value="{{ $user->idUsuari }}" <?php if( in_array($user->nomUsuari, $revidoresActuales ) ) echo "selected" ?>>{{ $user->nomUsuari}}</option>
 							@endforeach
 						</select>
 					</div>
+				</div>
 
+				<div class="modal-footer">
 					<div class="form-group">
-						<button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+						<button type="submit" class="btn btn-primary">
 							Guardar Plantilla
 						</button>
 					</div>
-					{{-- TODO: Cerrar formulario --}}
-				</form>
+				</div>
+			</form>
 
-			</div>
 		</div>
 
 
